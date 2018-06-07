97a6e6f217151dfd2caecb9da74cf39da342a704
wangyiping <wangyiping@4paradigm.com>
2018-06-07 12:27:11 +0800

    check container ENV opt

--- image-hub/start.sh
+++ image-hub/start.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
+check_opts() {
+    if [ -z "$REGISTRY" ]; then
+        echo "REGISTRY not set" && exit
+    fi
+
+    if [ -z "$USERNAME" ]; then
+        echo "USERNAME not set"  && exit
+    fi
+
+    if [ -z "$PASSWORD" ]; then
+        echo "PASSWORD not set"  && exit
+    fi
+}
+
+check_opts
 
 pecan serve /app/etc/pecan/config.py
 
